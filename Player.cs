18eaf0853d02858b335d354010a215bfa268d41d
Dáire McDermott <dairemcdermott@outlook.com>
2023-12-14 23:41:30 +0000

    adds event functions

--- Player.cs
+++ Player.cs
@@ -5,14 +5,51 @@ namespace Pong
 {
 	public partial class Player : Area2D
 	{
-		// Called when the node enters the scene tree for the first time.
 		public override void _Ready()
 		{
+			_screenSize = GetViewportRect().Size;
 		}
 
-		// Called every frame. 'delta' is the elapsed time since the previous frame.
 		public override void _Process(double delta)
 		{
+			var velocity = Vector2.Zero; // The player's movement vector.
+
+			if (Input.IsActionPressed("move_right"))
+			{
+				velocity.X += 1;
+			}
+
+			if (Input.IsActionPressed("move_left"))
+			{
+				velocity.X -= 1;
+			}
+
+			if (Input.IsActionPressed("move_down"))
+			{
+				velocity.Y += 1;
+			}
+
+			if (Input.IsActionPressed("move_up"))
+			{
+				velocity.Y -= 1;
+			}
+
+			var animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+
+			if (velocity.Length() > 0)
+			{
+				velocity = velocity.Normalized() * Speed;
+				animatedSprite2D.Play();
+			}
+			else
+			{
+				animatedSprite2D.Stop();
+			}
 		}
+
+		[Export]
+		private int Speed { get; set; } = 400;
+
+		private Vector2 _screenSize;
 	}
 }
